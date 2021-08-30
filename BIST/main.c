beb221d7ee1e3d7479ddef471618cc1dca1cba0d
prajwal <prajwal@ee.iitb.ac.in>
2021-08-30 12:11:26 +0530

    BIST started

---
+++ BIST/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// #include "core_portme.h"
+// #include "ajit_access_routines.h" 
+#include <string.h>
+
+char instr_types[5][20] = {"control_transfer", "data_transfer", "floating_point", "integer_alu", "misc"};
+char control_instrs[]
+
+int main () {
+
+    // ee_printf("Hey There");
+    return(1);
+}
