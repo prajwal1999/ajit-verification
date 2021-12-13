74c57adb3c13b63bbfab39d761e9cc720817366a
shailee-suryawanshi <sshailee270@gmail.com>
2021-12-13 11:05:46 +0530

    Combination of hits and misses

--- mem_tests/miss_ratio_tests/64_bit/BIGMEM/clean.sh
+++
@@ -1,3 +0,0 @@
-rm -rf  sparc-*
-rm -f *mmap *objdump *vars *elf __gen*.s custom* *.hex *.remapped   *.log all_kinds*.s 
-rm -f *.bin *bootstrap*lnk
--- mem_tests/timevs_blocksize/64_bit/SMALLMEM/clean.sh
+++
@@ -1,2 +0,0 @@
-rm -rf  sparc-*
-rm -f *mmap *objdump *vars *elf __gen*.s custom* *.hex *.remapped  
